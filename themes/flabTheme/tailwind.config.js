f84f5d144fb6843f8410c90d048475bac9594541
Filip Aben <filip@aben.be>
2022-04-13 21:07:07 +0200

    fix: add .gitignore + add missing files

---
+++ themes/flabTheme/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "layouts/**/*.html"
+  ],
+  theme: {
+    extend: {
+      "colors": {
+        'primary': 'hsla(197,46%,37%,1)',
+        'primary-light': 'hsla(185,43%,95%,1)'
+      }
+    },
+  },
+  plugins: [],
+}
